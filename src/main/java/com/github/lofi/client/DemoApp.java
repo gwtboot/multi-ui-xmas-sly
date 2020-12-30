befc301505a3685d1c40dee046f2335df643aa94
lofidewanto <lofidewanto@gmail.com>
2020-12-30 19:25:01 +0100

    update Singleton

--- src/main/java/com/github/lofi/client/DemoApp.java
+++ src/main/java/com/github/lofi/client/DemoApp.java
@@ -1,13 +1,18 @@
 package com.github.lofi.client;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 import com.dncomponents.bootstrap.client.BootstrapUi;
 import com.dncomponents.client.components.core.AppTemplates;
 import com.dncomponents.client.views.Ui;
 
 import elemental2.dom.DomGlobal;
 
+@Singleton
 public class DemoApp {
 
+	@Inject
 	public DemoApp() {
 		// Domino
 		// Layout.create("Hello Many Worlds!").show();
